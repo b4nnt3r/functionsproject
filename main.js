1a4e5fe8212638e04cf09a6f6d14a95f0baceb7d
Brandon Walden <brandonshanewalden@gmail.com>
2017-07-06 17:04:05 -0400

    fixed answer 2

--- main.js
+++ main.js
@@ -15,16 +15,16 @@ console.log(max(5, 10));
 // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
 // Then, write and example of using the function.
 
-function maxOfThree(num3, num4, num5) {
-  if (num3 > (num4 && num5)) {
+function maxOfThree(num1, num2, num3) {
+  if (num1 > num2 && num1 > num3) {
+    return num1;
+  } else if (num2 > num1 && num2 > num3) {
+    return num2;
+  } else if (num3 > num1 && num3 > num2){
     return num3;
-  } else if (num4 > (num3 && num5)) {
-    return num4;
-  } else {
-    return num5;
-  }
 }
-console.log(maxOfThree(25, 15, 105));
+}
+console.log(maxOfThree(5, 10, 15));
 
 // 3.
 // Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
@@ -86,7 +86,7 @@ function greet(param) {
   return "Hello, " + param + "!";
 }
 
-console.log(greet ("Heather"))
+console.log(greet("Heather"))
 
 // 9.
 // Write a function called `madlib` that takes 4 or more parameters (words).
@@ -97,8 +97,8 @@ console.log(greet ("Heather"))
 // sentence: "quick brown fox jumps over the fence"
 // Then, write and example of using the function.
 
-function madlib (noun1, noun2, verb1, adj1, adj2) {
+function madlib(noun1, noun2, verb1, adj1, adj2) {
   return "The " + adj1 + " " + noun2 + " " + verb1 + " " + "over the " + adj2 + " " + noun1 + "!";
 }
 
-console.log(madlib ("ferriswheel", "gopher", "leaped", "hairy", "giant"));
+console.log(madlib("ferriswheel", "gopher", "leaped", "hairy", "giant"));
